09683d24b71e0a978f6fd367c7c4c34e37291d3b
Even Grøtte <eogrotte@stud.ntnu.no>
2016-03-03 10:51:49 +0100

    Added a 'setTime' method to the Distance class.

--- src/main/Distance.java
+++ src/main/Distance.java
@@ -6,6 +6,9 @@ package main;
 
 public class Distance implements SensorInterface{
 
+    private float distance = 0;
+    private float time = (float)0.6;
+
     //the method takes in the distance measured in seconds (travel-time for the ultrasound) converts it into meters.
     public static float distance(float time){
         float distance;
@@ -13,8 +16,14 @@ public class Distance implements SensorInterface{
         return distance;
     }
 
+    //This method sets a new time-measurement from the distance sensor. Time is measured in seconds
+    public void setTime(float newTime){
+        this.time=newTime;
+    }
+
     @Override
     public void update() {
+        distance = distance(time);
 
     }
 
