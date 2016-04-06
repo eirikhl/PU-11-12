574dd9bc58b1ff7a223c12e96f9b4ed716328aa2
Sondre Jahrsengene <mjupithemop@gmail.com>
2016-04-06 11:19:20 +0200

    Fixed some values and equations

--- src/Sensors/WeatherToCoefficient.java
+++ src/Sensors/WeatherToCoefficient.java
@@ -6,39 +6,45 @@ public class WeatherToCoefficient implements SensorInterface{
     private float coefficient;
     private boolean auto; // settes når bruker velger om koeffisienten skal settet automatisk (basert på værmelding) eller manuelt
     private int postalNumber;
+    private int ticks;
+    private XmlParser parser;
 
     @SuppressWarnings("OctalInteger")
     public WeatherToCoefficient(){
         postalNumber = 0000;
         coefficient = 0.8f;
+        ticks = 0;
+        parser = new XmlParser();
     }
 
     public void setAuto(boolean auto) {
         this.auto = auto;
     }
 
-    private void setCoefficient(float pre, float temp){
+    private void setCoefficient(float[] weather){
         //Starts with 0.8 as it's the standard coefficient for summer driving
+        float temp = weather[0];
+        float pre = weather[1];
         float temporaryCo = 0.8f;
         //Uses precipitation to potentially subtract from coefficient
-        if (temp >= 0.0f){
+        if (temp >= 2.0f){
             if(pre > 4.2f){
-                temporaryCo -= 0.4f;
+                temporaryCo = 0.4f;
             }
-            else if(pre >1.6f){
-                temporaryCo -= 0.2f;
+            else if(pre > 1.6f){
+                temporaryCo = 0.6f;
             }
             else if (pre > 0.0f){
-                temporaryCo -= 0.1f;
+                temporaryCo = 0.7f;
             }
         }
         else{
             temporaryCo = 0.4f;
             if(temp < -10.0f){
-                temporaryCo -= 0.2f;
+                temporaryCo = 0.2f;
             }
             else if (temp < -5.0f){
-                temporaryCo -= 0.1f;
+                temporaryCo = 0.3f;
             }
         }
 
@@ -46,6 +52,11 @@ public class WeatherToCoefficient implements SensorInterface{
     }
 
     public void update(){
+        ticks += 1;
+        if (ticks == 72000){
+            ticks = 0;
+            //setCoefficient(parser.parse()); Må fikses når xmlparser er ferdig
+        }
         // URL url = new URL("http://www.yr.no/sted/Norge/postnummer/"+getPostalNumber()+"/varsel.xml");
 //        setAuto(true);//for testing-purposes
 //        float weathercoefficient;
