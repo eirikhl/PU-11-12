314fd621d29167ee13bcd444815d61de13d69d93
Hallvard <hallvard95@gmail.com>
2016-04-06 11:20:13 +0200

    not much

--- src/weatherBusiness/XmlTesterHallvard.java
+++ src/weatherBusiness/XmlTesterHallvard.java
@@ -1,6 +1,8 @@
 package weatherBusiness;
 
 import java.io.File;
+import java.io.InputStream;
+import java.net.URL;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
 import org.w3c.dom.Document;
@@ -11,6 +13,17 @@ import org.w3c.dom.Element;
 public class XmlTesterHallvard {
 
     public static void main(String[] args) {
+
+        try {
+            URL url = new URL("http://www.xyz.com/book.xml");
+            InputStream stream = url.openStream();
+            DocumentBuilderFactory dbFactory
+                    = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = dbFactory.newDocumentBuilder();
+            Document doc = docBuilder.parse(stream);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         try {
             File inputFile = new File("D:\\Documents\\TDT4140 - PU\\IJ\\PU-11-12\\src\\weatherBusiness\\varsel.xml");
