7f57608b1b62dde2543f5714a8c0b1ced00068e0
Eirik <eirikhl@stud.ntnu.no>
2016-04-20 20:21:07 +0200

    Very minor cleanup

--- src/Sensors/MockSpeed.java
+++ src/Sensors/MockSpeed.java
@@ -1,16 +1,9 @@
 package Sensors;
 
-/**
- * Created by Eirik on 20.04.2016.
- */
 public class MockSpeed implements SensorInterface {
-    @Override
-    public void update() {
+    public void update() {}
 
-    }
-
-    @Override
     public float getData() {
-        return 70;
+        return 70.0f;
     }
 }
